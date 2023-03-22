aa09bd325a25f2b79371b664fef645c032055d1e
andre <andre-khan@yandex.ru>
2023-03-22 21:35:15 +0300

    Метод для перебора массива по условиям задачи

--- program/Program.cs
+++ program/Program.cs
@@ -4,3 +4,18 @@
     for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
     Console.WriteLine();
 }
+
+void CreateOutputArray(string[] OutputArray)
+{
+    Console.Write($"Output array: ");
+    string[] newarr = new string[OutputArray.Length];
+    for (int i = 0; i < OutputArray.Length; i++)
+    {
+        if (OutputArray[i].Length <= 3)
+        {
+            newarr[i] = OutputArray[i];
+            Console.Write($"{newarr[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
